b776a92e2b56fcecb09b88b15423952f107b61a9
Dannschz <deenkes94@gmail.com>
2019-08-08 15:26:24 -0500

    no mucho

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', e => {
     document.getElementById('button').addEventListener('click', e => {
         console.log('Me has clickado');
+        alert('Muy Bien!');
     });
 });
